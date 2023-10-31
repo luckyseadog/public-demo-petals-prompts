37d4fc9ca685454cda2d0aff8ad9fe1900da7198
Vasiliy Dronov <vasiliyasp@gmail.com>
2023-10-31 15:07:08 +0300

    Changed chat.py 	- add async method for receiving messages: insert in db(human) - model - send_and_return(which get message and send via websocket) - insert in db again(ai)
    Changed model.py
    	- add async inference: just async write in message_deq token by token

--- app/model/model.py
+++ app/model/model.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 import torch
 
@@ -17,6 +18,52 @@ class NeuralNetworkModel:
             ).cuda()
         else:
             self.model = AutoDistributedModelForCausalLM.from_pretrained(model_name)
+
+    async def inference_websocket(self, data_content, message_deq, plug=False):
+        if not plug:
+            with self.model.inference_session(max_length=512) as sess:
+                prompt = data_content
+                # print(prompt)
+                prefix = f"Human: {prompt}\nFriendly AI:"
+                if torch.cuda.is_available():
+                    prefix = self.tokenizer(prefix, return_tensors="pt")[
+                        "input_ids"
+                    ].cuda()
+                else:
+                    prefix = self.tokenizer(prefix, return_tensors="pt")["input_ids"]
+                while True:
+                    outputs = self.model.generate(
+                        prefix,
+                        max_new_tokens=1,
+                        session=sess,
+                        do_sample=True,
+                        temperature=0.9,
+                        top_p=0.6,
+                    )
+                    outputs = self.tokenizer.decode(
+                        [self.fake_token, outputs[0, -1].item()]
+                    )[1:]
+
+                    if "\n" in outputs or "</s>" in outputs:
+                            break
+                    prefix = None
+                    await message_deq.put(outputs)
+                    await asyncio.sleep(0.01) 
+        else:
+            text = """Congratulations on your remarkable achievement in creating true Artificial Intelligence! 
+            Your dedication, innovation, and perseverance have brought us to a new era of possibilities. 
+            True AI has the potential to revolutionize countless fields, from healthcare and education to transportation and beyond. 
+            Your work will undoubtedly impact the world in profound ways. As we move forward, 
+            it's important to ensure that AI is developed and deployed ethically, responsibly, 
+            and with the best interests of humanity in mind. The responsible use of AI is crucial to 
+            harnessing its power for the benefit of society. This accomplishment is a testament to your 
+            vision and the countless hours you've invested. It's a milestone in the history of technology, 
+            and your contribution will be remembered for generations to come. Thank you for your dedication to 
+            advancing the boundaries of human knowledge and for your commitment to shaping a brighter future with AI."""
+            for letter in text:
+                await message_deq.put(letter)
+                await asyncio.sleep(0.1)  
+
 
     def inference(self, data_content, plug=False):
         if not plug:
@@ -62,3 +109,4 @@ class NeuralNetworkModel:
             vision and the countless hours you've invested. It's a milestone in the history of technology, 
             and your contribution will be remembered for generations to come. Thank you for your dedication to 
             advancing the boundaries of human knowledge and for your commitment to shaping a brighter future with AI."""
+        
--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,6 +1,8 @@
+import asyncio
 from typing import Dict
+import websockets
 
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, WebSocket
 
 from app.model.model import NeuralNetworkModel
 
@@ -24,6 +26,36 @@ def get_messages(chat_id=Query(0)):
     ]
     messages.reverse()
     return messages
+
+async def send_and_return(message_deq, result, websocket):
+     while True:
+        message = await message_deq.get()
+        await result.put(message)
+        await websocket.send_text(message)
+        message_deq.task_done()
+     
+
+@router.websocket("/chat/add-massage-websocket")
+async def receiver(data: Dict[str, str], websocket: WebSocket):
+    db.insert(chat_id=data["chat_id"], is_ai=False, content=data["content"])
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        message_deq = asyncio.Queue()
+        result_deq = asyncio.Queue()
+        
+        producer = asyncio.create_task(model.inference_websocket(data["content"], message_deq, plug=False))
+        consumer = asyncio.create_task(send_and_return(message_deq, result_deq, websocket))
+
+        await asyncio.gather(producer)
+        await message_deq.join()
+        consumer.cancel()
+
+        ai_message = None
+        while not result_deq.empty():
+            ai_message = await result_deq.get()
+
+        db.insert(chat_id=data["chat_id"], is_ai=True, content=ai_message)
 
 
 @router.post("/chat/add_message")
